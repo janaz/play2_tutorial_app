6cd7bdcc807fb3e98046c2317a558d5571c4f07d
Tomasz Janowski <tomasz.janowski@rea-group.com>
2013-10-29 11:17:32 +1100

    reverted broken evolution

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,20 +3,11 @@
 
 # --- !Ups
 
-create table csv_file (
-  id                        bigint not null,
-  file_name                 varchar(255),
-  owner_id                  bigint,
-  constraint pk_csv_file primary key (id))
-;
-
 create table task (
   id                        bigint not null,
   name                      varchar(255),
   constraint pk_task primary key (id))
 ;
-
-create sequence csv_file_seq;
 
 create sequence task_seq;
 
@@ -27,13 +18,9 @@ create sequence task_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists csv_file;
-
 drop table if exists task;
 
 SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists csv_file_seq;
 
 drop sequence if exists task_seq;
 
