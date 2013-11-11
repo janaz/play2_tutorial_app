067ba874664f280320002acdbe5f8edd6d11c63c
Tomasz Janowski <tomasz.janowski@rea-group.com>
2013-11-11 13:26:50 +1100

    cleanup

--- app/com/clustrino/csv/PersistException.java
+++ app/com/clustrino/csv/PersistException.java
@@ -1,14 +1,5 @@
 package com.clustrino.csv;
 
-import java.io.IOException;
-
-/**
- * Created with IntelliJ IDEA.
- * User: tomasz.janowski
- * Date: 10/11/13
- * Time: 9:58 PM
- * To change this template use File | Settings | File Templates.
- */
 public class PersistException extends Exception {
     public PersistException(Throwable e) {
         super(e);
